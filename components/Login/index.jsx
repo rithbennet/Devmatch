ef10eef655599519df78f2aeb1c459fb59493ca9
chriszlnn <chrislynjules@gmail.com>
2024-08-17 10:40:48 +0800

    NewPage

---
+++ components/Login/index.jsx
@@ -0,0 +1,17 @@
+import styles from "./styles.module.scss";
+
+/**
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+export default function login (){
+    return (
+
+        <div className={styles.container}>
+            <h1>Login</h1>
+        </div>
+
+    );
+}
---
+++ components/welcome/index.jsx
@@ -0,0 +1,14 @@
+import styles from "./styles.module.scss";
+
+/**
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function Welcome() {
+    return(
+        <div>
+            <h1>Welcome</h1>
+        </div>
+    )
+}
